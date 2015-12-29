775181e507030bf50fd1c91bc1b841f8405664eb
Bryan Jennings <bryanjenningz@gmail.com>
2015-12-28 18:41:38 -0800

    convert to jquery, add ajax request to youtube

--- ad-free-yt.js
+++ ad-free-yt.js
@@ -1,11 +1,19 @@
-(function() {
-  var searchForm = document.querySelector('.search-form');
-  var searchBox = searchForm.querySelector('.search-box');
-  searchForm.addEventListener('submit', search);
+var searchForm = $('.search-form');
+var searchBox = $('.search-box');
+searchForm.submit(search);
 
-  function search(event) {
-    event.preventDefault();
-    console.log(searchBox.value);
-    searchBox.value = '';
-  }
-})();
+function search(event) {
+  event.preventDefault();
+  var url = "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&jsonp=loadVideos&q=" 
+    + encodeURIComponent(searchBox.val()) + "&cp=1";
+  $.ajax({
+    type: "GET",
+    url: url,
+    dataType: "script"
+  });
+  searchBox.val('');
+}
+
+function loadVideos(data) {
+  console.log(data);
+}
